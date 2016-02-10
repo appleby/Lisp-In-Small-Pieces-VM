8f249f48794ee8afeff0a3b75d293fc4eed16a57
Mike Appleby <mike@app.leby.org>
2016-02-09 22:14:09 -0600

    Set ATLAS_NAME = "lisp-in-small-pieces-vm".

--- atlas_env.sh
+++ atlas_env.sh
@@ -1,3 +1,3 @@
 # Source this file to define the env vars required for the build.
 export ATLAS_USERNAME=appleby
-export ATLAS_NAME=archlinux-x86_64-base
+export ATLAS_NAME="lisp-in-small-pieces-vm"
