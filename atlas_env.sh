4e6ac3711a6f4df975112f698c594e6bd27c9b8e
Mike Appleby <mike@app.leby.org>
2016-02-09 21:41:22 -0600

    Set ATLAS_NAME = archlinux-x86_64-base.

--- atlas_env.sh
+++ atlas_env.sh
@@ -1,3 +1,3 @@
 # Source this file to define the env vars required for the build.
 export ATLAS_USERNAME=appleby
-export ATLAS_NAME="archlinux-x86_64-base-test"
+export ATLAS_NAME=archlinux-x86_64-base
