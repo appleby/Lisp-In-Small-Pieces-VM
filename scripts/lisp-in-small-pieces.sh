e8fa5c82ce77c724bc6dbaae558687e0fa46daf4
Mike Appleby <mike@app.leby.org>
2016-02-09 22:31:59 -0600

    Add script to install lisp-in-small-pieces deps.

---
+++ scripts/lisp-in-small-pieces.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash -x
+
+echo "==> Installing git gambit-c and bigloo"
+/usr/bin/pacman -S --noconfirm git gambit-c bigloo
+
+echo "==> Creating tmpdir"
+tmpdir=$(/usr/bin/mktemp --directory)
+pushd ${tmpdir}
+
+echo "==> Installing cower"
+/usr/bin/curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz
+/usr/bin/tar xf cower.tar.gz
+
+pushd cower
+/usr/bin/gpg --recv-key 487EACC08557AD082088DABA1EB2638FF56C0C53
+/usr/bin/makepkg -scri --noconfirm
+popd
+
+echo "==> Installing mit-scheme"
+/usr/bin/cower -d mit-scheme
+pushd mit-scheme
+/usr/bin/makepkg -scri --noconfirm
+popd
+popd # Pop out of tmpdir
+
+echo "==> Removing tmpdir"
+rm -rf ${tmpdir}
