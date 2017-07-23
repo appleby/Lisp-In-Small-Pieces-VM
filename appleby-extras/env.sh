955b53e8e4e10719605af461c95834b91a79bad7
Mike Appleby <mike@app.leby.org>
2017-07-23 13:11:49 -0500

    Remove atlas features and upload box to vagrant-cloud

--- appleby-extras/env.sh
+++ appleby-extras/env.sh
@@ -1,3 +1,3 @@
 # Source this file to define the env vars required for the build.
-export ATLAS_USERNAME=appleby
-export ATLAS_NAME=archlinux-x86_64-base
+export VAGRANT_CLOUD_USERNAME=appleby
+export VAGRANT_CLOUD_NAME=archlinux-x86_64-base
