0dee9a47ddf6338c6be35ca6c55b79f2dd70cc23
Mike Appleby <86076+appleby@users.noreply.github.com>
2020-10-08 19:06:10 -0500

    Drop gambit-c and install mit-scheme from local PKGBUILD
    Gambit has a bug [1] that prevents using define-syntax, and the
    version of mit-scheme in arch community repo hash a bug related to
    printing vectors [2].
    
    The included PKGBUILD is just the one from the arch community repo,
    updated to the latest stable mit-scheme release (10.1.11).
    
    [1]: https://github.com/gambit/gambit/issues/384
    [2]: https://lists.gnu.org/archive/html/mit-scheme-devel/2020-01/msg00000.html
    
    Acked-by: Mike Appleby <86076+appleby@users.noreply.github.com>

--- scripts/lisp-in-small-pieces.sh
+++ scripts/lisp-in-small-pieces.sh
@@ -5,12 +5,18 @@ set -e
 echo "==> Setting hostname"
 echo lisp-in-small-pieces-vm | sudo tee /etc/hostname > /dev/null
 
-echo "==> Installing git gambit-c bigloo indent and time"
-sudo /usr/bin/pacman -S --noconfirm git mit-scheme gambit-c bigloo indent time
+echo "==> Installing git bigloo indent and time"
+sudo /usr/bin/pacman -S --noconfirm git bigloo indent time
 
 echo "==> Creating tmpdir"
 tmpdir=$(/usr/bin/mktemp --directory --tmpdir=${HOME})
 pushd "$tmpdir"
+
+echo "==> Installing mit-scheme"
+# Chris Hanson <cph@chris-hanson.org>
+/usr/bin/gpg --keyserver keys.gnupg.net --recv-keys C9E40BAAFD0CB132
+/usr/bin/mv /tmp/PKGBUILD .
+/usr/bin/makepkg -scri --noconfirm
 
 echo "==> Installing auracle"
 /usr/bin/curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/auracle-git.tar.gz
